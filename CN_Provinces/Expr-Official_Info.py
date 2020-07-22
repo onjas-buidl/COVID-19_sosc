7abfcee0b874806063eacc9a7a20090d5c92b7f3
Qitian Hu <jasonhu@uchicago.edu>
2020-07-22 16:28:37 +0800

    finish aggregating data to 疫情-20市委书记-V2
    prepare for more stuff

--- CN_Provinces/Expr-Official_Info.py
+++ CN_Provinces/Expr-Official_Info.py
@@ -1,16 +1,22 @@
+"""这文档主要
+1. 搞2020市委书记数据集
+2. 探索市委书记年龄
+"""
+
+# %%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 from tqdm import tqdm
 
-
+# %% initial analysis
 cs = pd.read_excel('CN_Provinces/市领导数据/市委书记.xlsx')
 cs = cs[cs.year == 2018]
 cs['birthmonth'] = cs.bmymager.apply(pd.to_datetime)
 cs['age'] = (pd.to_datetime('2018-02-01') - cs['birthmonth']).astype('<m8[Y]')
 # cs['birth'] = cs.bmymager.apply(pd.to_datetime)
-cs.to_excel('CN_Provinces/CN_Policy/2018-all_secretary_info.xlsx')
+cs.to_excel('CN_Provinces/CN_Policy/2018-all_secretary_info.xlsx', index=False)
 cs.age.mean()
 
 cs.age.quantile(.1)
@@ -53,7 +59,7 @@ for n in ['personid',
 newcs.year = 2020
 
 # newcs.loc[newcs.existing == 1111, 'bmymager'] = 0
-newcs.to_excel('CN_Provinces/市领导数据/市委书记_2020-V0.5.xlsx')
+newcs.to_excel('CN_Provinces/市领导数据/市委书记_2020-V0.5.xlsx', index=False)
 
 # TODO: 还要把我在封城里面搞的加上去！即locked_down为True的人：需要替换！
 
@@ -107,7 +113,7 @@ for ct_name in tqdm(todo_ct):
 		error_list.append(ct_name)
 # %%
 error_list.append('永州市')
-cs_20_full.to_excel('CN_Provinces/市领导数据/wiki-20市委书记-322ct.xlsx')
+cs_20_full.to_excel('CN_Provinces/市领导数据/wiki-20市委书记-322ct.xlsx', index=False)
 # %%
 cs_20_full = pd.read_excel('CN_Provinces/市领导数据/wiki-20市委书记-322ct.xlsx')
 # one error corrected for 沈阳 in Excel -- many fields
@@ -123,7 +129,7 @@ cs_20_full['race'] = cs_20_full.race.apply(lambda x: x.replace('\n', '').replace
 cs_20_full['inaug_time'] = \
 	cs_20_full.inaug_time.apply(lambda x: x.replace('年', '-').replace('月', '').replace('4日', '').replace('7日', '').replace('\n', ''))
 cs_20_full.drop('Unnamed: 0', axis=1, inplace=True)
-cs_20_full.to_excel('CN_Provinces/市领导数据/wiki-20市委书记-322ct-V2.xlsx')
+cs_20_full.to_excel('CN_Provinces/市领导数据/wiki-20市委书记-322ct-V2.xlsx', index=False)
 
 
 
@@ -138,14 +144,95 @@ for ct_name in error_list:
 	cs20.loc[cs20.ctnm == ct_name, 'provincename'] = cs.loc[cs.ctnm == ct_name].provincename.values[0]
 
 # sum(cs20.inaug_time.apply(pd.to_datetime) > pd.to_datetime('Feb 01, 2020'))
-cs20.to_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V1.xlsx')
+cs20.to_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V1.xlsx', index=False)
 
 
-# %% 替换疫情中的官员
+# %% Combine & Produce final aggregate dataset
 """
 主要issue是2020.2：到底算不算决定lockdown policy的人呢
 非武汉城市的lockdown是1.25 - 2.13
 	一月份那几位的上任都在这个时段之前，所以可以算进去
 """
+cs20 = pd.read_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V1.xlsx')
+cs20 = cs20[cs20.rule_in_covid]
+cs20['age_feb20'] = (pd.to_datetime('2020-02-01') - cs20['birthmonth'].apply(pd.to_datetime)).astype('<m8[Y]')
+pc = pd.read_excel('CN_Provinces/CN_Policy/V4-Yuhang_Pan-CN_lockdown_data加官员信息.xlsx')
+cs20['locked_down'] = cs20.citycode.apply(lambda x: x/100 in l)
+# cs20['lc_date'] = None
+pc_need = pc[['daySinceFirstCase']]
+pc_need['citycode'] = pc.city_code2010 * 100
+pc_need['lockdown_date'] = pc.lockdown
+cs20 = pd.merge(cs20, pc_need, on='citycode', how='left')
+
+cs20.to_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V2.xlsx', index=False)
+
+
+# %% 把全部官员替换疫情中的官员，并检测是否有age difference
+cs20 = pd.read_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V2.xlsx')
+cs20 = cs20[cs20.provincecode != 420000]  # 只看非湖北的
+cs20_lc = cs20[cs20.locked_down]
+cs20_lc.age_feb20.mean()
+cs20[~cs20['locked_down']].age_feb20.mean()
+
+
+lc_mean = cs20_lc.age_feb20.mean()
+c = 0
+n = 100000
+l = list(cs20.age_feb20)
+for i in tqdm(range(n)):
+	if np.mean(random.choices(l, k=90)) >= lc_mean:
+		c += 1
+
+print('p-value is:', c/n)  # it's fucking significant!
+
+
+
+# %% further Histograms
+cs20 = pd.read_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V2.xlsx')
+cs20 = cs20[cs20.provincecode != 420000]  # 只看非湖北的
+
+cs20.age_feb20.hist(bins=17)
+cs20_lc.age_feb20.hist(bins=10, color='red')
+plt.title('Age distribution of officials: total vs. lock-down officials')
+plt.show()
+# plt.savefig('CN_Provinces/市领导数据/Exports/Age_distrib_by_lockdown.png')
+# %% Percentage by age group
+from collections import Counter
+np.histogram(cs20.age_feb20)
+# plot the age group lockdown
+c_lk = dict(Counter(cs20_lc.age_feb20))
+c_all = dict(Counter(cs20.age_feb20))
+
+# c_lk = np.histogram(cs20_lc.age_feb20, bins=10)
+# c_all = np.histogram(cs20.age_feb20, bins=17)
+
+age_range = [i for i in range(44, 62)]
+percentages = []
+for i in age_range:
+	if i not in c_lk.keys():
+		percentages.append(0)
+	else:
+		percentages.append(c_lk[i] / c_all[i])
+
+plt.bar(x=age_range, height=percentages)
+# plt.show()
+plt.title('Percentage of officials that choosed lockdown vs. Age group')
+plt.show()
+# plt.savefig('CN_Provinces/市领导数据/Exports/pct_lck_vs_age.png')
+
+
+
+
+
+# %% More qualitative exploration
+cs20 = pd.read_excel('CN_Provinces/市领导数据/疫情-20市委书记-331ct-V2.xlsx')
+cs20.sort_values('provincename', inplace=True)
+cols = cs20.columns.tolist()
+cols = [cols[-2]] + cols[:-2] + [cols[-1]]
+cs20 = cs20[cols]
+# Ass
+
+
+
 
 
