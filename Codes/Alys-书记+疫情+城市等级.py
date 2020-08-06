52b365dc0120e4ad6da70cd069869a478bdc6cbf
Qitian Hu <jasonhu@uchicago.edu>
2020-08-06 10:39:35 +0800

    clean up reference

--- Codes/Alys-书记+疫情+城市等级.py
+++ Codes/Alys-书记+疫情+城市等级.py
@@ -77,12 +77,12 @@ for fix_date in dates[10:]:
 	data['case_on_date'] = data[fix_date]
 	data['case_per_10k'] = data['case_on_date'] / data['popHR18_all']
 	data_ = data[~data['自治州-盟-地区']]
-	# results = smf.ols('locked_on_date ~ case_on_date + gdp2018 +' +
-	#         'Log_popHR18_all + second_ind + third_ind + ' +
-	#         'sub_prov_ct + age_feb20 + tenure', data=data_).fit()
-	results = smf.ols('locked_on_date ~ case_per_10k + gdp2018 +' +
-	                  ' second_ind + third_ind + ' +
-	                  'sub_prov_ct + age_feb20 + tenure', data=data_).fit()
+	results = smf.ols('locked_on_date ~ case_on_date + gdp2018 +' +
+	        'Log_popHR18_all + second_ind + third_ind + ' +
+	        'sub_prov_ct + age_feb20 + tenure', data=data_).fit()
+	# results = smf.ols('locked_on_date ~ case_per_10k + gdp2018 +' +
+	#                   ' second_ind + third_ind + ' +
+	#                   'sub_prov_ct + age_feb20 + tenure', data=data_).fit()
 	# results.summary()
 	# res = mod.fit()
 
