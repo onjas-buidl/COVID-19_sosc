665df3dbf3a51044d29171017c6a94cc728548c8
Qitian Hu <jasonhu@uchicago.edu>
2020-08-31 20:29:33 +0800

    用tableau 探索bdidx

--- Data/Pop_Movement/Calc-deg_of_control.py
+++ Data/Pop_Movement/Calc-deg_of_control.py
@@ -8,15 +8,13 @@ matplotlib.rcParams['axes.unicode_minus'] = False # in case minus sign is shown 
 bd = pd.read_excel('Data/Pop_Movement/baidu_index-2019~20/Data/migrate_Trend.xlsx',
                    sheet_name='internalflowhistory')
 
-bd = pd.read_excel('Data/Pop_Movement/baidu_index-2019~20/Data/migrate_Trend.xlsx',
-                   sheet_name='internalflowhistory')
 bd.dropna(inplace=True)
 bd.rename(columns={'Unnamed: 0': 'name', 'Unnamed: 1': 'type'}, inplace=True)
 bd.loc[bd.name == '吉林市', 'name'] = '吉林'
 bd['name'] = bd.name.apply(lambda x: x[:-2] if x[-2:] == '地区' else x)
+bd.rename(columns={'name': 'ct_shortname'}, inplace=True)
 
 dates = ['20200124', '20200125', '20200126', '20200127', '20200128', '20200129', '20200130', '20200131', '20200201', '20200202', '20200203', '20200204', '20200205', '20200206', '20200207', '20200208', '20200209', '20200210', '20200211', '20200212', '20200213', '20200214', '20200215', '20200216', '20200217', '20200218', '20200219', '20200220', '20200221', '20200222', '20200223', '20200224', '20200225', '20200226', '20200227', '20200228', '20200229']
-# dates19 = [i.replace('2020', '2019') for i in dates]
 dates19 = ['20190124', '20190125', '20190126', '20190127', '20190128', '20190129', '20190130', '20190131', '20190201', '20190202', '20190203', '20190204', '20190205', '20190206', '20190207', '20190208', '20190209', '20190210', '20190211', '20190212', '20190213', '20190214', '20190215', '20190216', '20190217', '20190218', '20190219', '20190220', '20190221', '20190222', '20190223', '20190224', '20190225', '20190226', '20190227', '20190228', '20190301']
 dates19_match = ['20190204', '20190205', '20190206', '20190207', '20190208', '20190209', '20190210', '20190211', '20190212', '20190213', '20190214', '20190215', '20190216', '20190217', '20190218', '20190219', '20190220', '20190221', '20190222', '20190223', '20190224', '20190225', '20190226', '20190227', '20190228', '20190301', '20190302', '20190303', '20190304', '20190305', '20190306', '20190307', '20190308', '20190309', '20190310', '20190311', '20190312']
 l = list(bd.columns)
@@ -56,8 +54,6 @@ for i in dates19_match:
 bd['19sum'] = bd['19sum'] / len(dates)
 bd['bd_feb15avgdif_holi'] = bd['19sum'] - bd['20sum']
 
-bd.rename(columns={'name': 'ct_shortname'}, inplace=True)
-
 bd['20sum'] = 0
 bd['19sum'] = 0
 for i in dates:
@@ -71,6 +67,37 @@ bd['bd_feb15avgdif_date'] = bd['19sum'] - bd['20sum']
 
 bd[['ct_shortname', 'bd_feb15avgdif_date', 'bd_feb15avgdif_holi',
     'bd_feb29avgdif_date', 'bd_feb29avgdif_holi']].to_csv('Data/Pop_Movement/百度迁徙平均政策强度.csv', index=False)
+# %% 每个城市每天的 difference
+dates = ['20200124', '20200125', '20200126', '20200127', '20200128', '20200129', '20200130', '20200131', '20200201', '20200202', '20200203', '20200204', '20200205', '20200206', '20200207', '20200208', '20200209', '20200210', '20200211', '20200212', '20200213', '20200214', '20200215', '20200216', '20200217', '20200218', '20200219', '20200220', '20200221', '20200222', '20200223', '20200224', '20200225', '20200226', '20200227', '20200228', '20200229']
+dates19 = ['20190124', '20190125', '20190126', '20190127', '20190128', '20190129', '20190130', '20190131', '20190201', '20190202', '20190203', '20190204', '20190205', '20190206', '20190207', '20190208', '20190209', '20190210', '20190211', '20190212', '20190213', '20190214', '20190215', '20190216', '20190217', '20190218', '20190219', '20190220', '20190221', '20190222', '20190223', '20190224', '20190225', '20190226', '20190227', '20190228', '20190301']
+dates19_match = ['20190204', '20190205', '20190206', '20190207', '20190208', '20190209', '20190210', '20190211', '20190212', '20190213', '20190214', '20190215', '20190216', '20190217', '20190218', '20190219', '20190220', '20190221', '20190222', '20190223', '20190224', '20190225', '20190226', '20190227', '20190228', '20190301', '20190302', '20190303', '20190304', '20190305', '20190306', '20190307', '20190308', '20190309', '20190310', '20190311', '20190312']
+
+holi = False
+if holi:
+    for i in range(len(dates)):
+        bd[str(i)] = bd[dates19_match[i]] - bd[dates[i]]
+else:
+    for i in range(len(dates)):
+        bd[str(i)] = bd[dates19[i]] - bd[dates[i]]
+
+bd = pd.merge(bd, cscv[['ct_shortname', 'prov']], on='ct_shortname', how='left')
+exp = bd[['ct_shortname', 'prov'] + [str(i) for i in range(len(dates))]]
+# exp.dropna(inplace=True)
+exp.to_csv('Data/Pop_Movement/city_bd_diff_holi.csv', index=False)
+
+if holi:
+    pd.melt(exp, id_vars=['ct_shortname', 'prov'], value_vars=None, var_name='Date',
+            value_name='value', col_level=None).to_csv('Data/Pop_Movement/city_bd_diff_holi-melt.csv', index=False)
+else:
+    pd.melt(exp, id_vars=['ct_shortname', 'prov'], value_vars=None, var_name='Date',
+            value_name='value', col_level=None).to_csv('Data/Pop_Movement/city_bd_diff_date-melt.csv', index=False)
+
+# %% 把每天的病毒数量也加上去？
+
+
+
+
+
 # %% plot to check -- 按公历日期
 # ctnm = '嘉兴'
 # city_name = '西宁'
@@ -110,5 +137,6 @@ bd[['ct_shortname', 'bd_feb15avgdif_date', 'bd_feb15avgdif_holi',
 # plt.xticks(rotation=45)
 # plt.title(city_name+' - 按照放假日期. Red: 2020 Blue: 2019')
 # plt.show()
+
 
 
